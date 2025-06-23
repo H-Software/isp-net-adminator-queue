b6bdcfa137ddb56f18ef8a87a329743b81de1a47
Patrik Majer <patrik.majer.pisek@gmail.com>
2025-06-23 23:17:14 +0200

    add tooling and CI stuff (#3)

--- internal/flag/flag.go
+++ internal/flag/flag.go
@@ -6,7 +6,7 @@ import (
 )
 
 var (
-	FlagRedisAddr        = flag.String("redis-addr", "localhost:16379", "Redis server address")
+	FlagRedisAddr        = flag.String("redis-addr", "localhost:6379", "Redis server address")
 	FlagGroupGracePeriod = flag.Duration("grace-period", 10*time.Second, "Group grace period")
 	FlagGroupMaxDelay    = flag.Duration("max-delay", 30*time.Second, "Group max delay")
 	FlagGroupMaxSize     = flag.Int("max-size", 3, "Group max size")
